fd71b44e70fc1c323ee778bfc9c3b7185ad60b47
amite <amit.erandole@gmail.com>
2017-06-06 10:51:14 +0530

    added navigation to link pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
+import Link from 'next/link'
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
         <div className="row">
-          
+
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+          <link href="https://fonts.googleapis.com/css?family=Bubbler+One" rel="stylesheet" />
+          <nav className="navbar navbar-default navbar-custom">
+            <div className="container-fluid">
+
+              <div className="navbar-header page-scroll">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span className="sr-only">Toggle navigation</span>
+                  Menu <i className="fa fa-bars"></i>
+                </button>
+                <a className="navbar-brand" href="/">React Next</a>
+              </div>
+
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav navbar-right">
+                  <li>
+                    <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <Link className="about-link" href="/about">
+                      <a>About</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <a href="post.html">Sample Post</a>
+                  </li>
+                  <li>
+                    <a href="contact.html">Contact</a>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+
+          </nav>
+
           <h1>As an interesting side note, as a head without a body, I envy the dead.</h1>
           <img src="./static/img/home-bg.jpg" className="img-responsive" />
           <br />
@@ -29,9 +65,8 @@ class Home extends Component {
 
         </div>
         <style>{`
-          body { 
-            background: yellow;
-            font: 11px menlo;
+          h1 { 
+            font-family: 'Bubbler One', sans-serif;
             color: #666;
           }
         `}</style>
