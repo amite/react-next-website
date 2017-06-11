6108b55e765a6f9892d83f1f0dd52e1388643d7b
amite <amit.erandole@gmail.com>
2017-06-11 09:36:27 +0530

    added logging

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+require('now-logs')('my-secret-key')
+
 app.prepare()
 .then(() => {
   const server = express()
